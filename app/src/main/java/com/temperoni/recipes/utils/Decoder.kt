5c0d0afe02bc6f1400f5152e94b9aa7607e6de18
Leandro Temperoni <leandrotemperoni@gmail.com>
2018-05-30 14:45:14 -0300

    Migrate last class

---
+++ app/src/main/java/com/temperoni/recipes/utils/Decoder.kt
@@ -0,0 +1,35 @@
+package com.temperoni.recipes.utils
+
+import com.google.gson.Gson
+import com.temperoni.recipes.domain.dto.Recipe
+
+import org.apache.commons.io.IOUtils
+
+import java.io.IOException
+
+/**
+ * @author Leandro Temperoni
+ */
+class Decoder {
+
+    fun decodeJson(fileName: String): List<Recipe>? {
+        return Gson().fromJson(getJson(fileName), RecipeList::class.java).recipes
+    }
+
+    private fun getJson(fileName: String): String {
+        var result = ""
+
+        val classLoader = javaClass.classLoader
+        try {
+            result = IOUtils.toString(classLoader.getResourceAsStream(fileName))
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        return result
+    }
+
+    private inner class RecipeList {
+        val recipes: List<Recipe>? = null
+    }
+}
